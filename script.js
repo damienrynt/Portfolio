93b0ecaddb81e9df4d61429e5f52d5334df03c88
damienrynt <149192477+damienrynt@users.noreply.github.com>
2025-10-17 13:52:11 -0400

    Add scroll event for additional rotating gear

--- script.js
+++ script.js
@@ -6,6 +6,12 @@ window.addEventListener("scroll", () => {
   gear.style.transform = `rotate(${rotation}deg)`;
 });
 
+const gears = document.querySelectorAll('.gear');
+window.addEventListener("scroll", () => {
+  const gear = document.querySelector(".rotating-gear2");
+  const rotation = window.scrollY * 0.5; // speed factor
+  gear.style.transform = `rotate(${rotation}deg)`;
+});
 // Smooth scrolling
 document.querySelectorAll('a[href^="#"]').forEach(anchor => {
   anchor.addEventListener('click', function (e) {
