d24fd1838b503a2f5817084150e52a878ec556f7
damienrynt <149192477+damienrynt@users.noreply.github.com>
2025-10-17 14:32:35 -0400

    Adjust gear2 rotation based on scroll position

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const gear2 = document.querySelector(".rotating-gear2");
 window.addEventListener("scroll", () => {
   const rotation = window.scrollY * 0.5;
   gear1.style.transform = `rotate(${rotation}deg)`;
-  gear2.style.transform = `rotate(${-rotation}deg)`;
+  gear2.style.transform = `rotate(${20-rotation}deg)`;
 });
 
 document.querySelectorAll('a[href^="#"]').forEach(anchor => {
